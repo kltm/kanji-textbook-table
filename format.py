9c5a9124cdcdb33b8ab115dcb0053d661b618d49
Seth Carbon <github.com@genkisugi.net>
2019-08-10 18:23:31 -0700

    first pass on possibly usable code

--- format.py
+++ format.py
@@ -13,6 +13,7 @@ import argparse
 import logging
 import csv
 import pystache
+import json
 
 ## Logger basic setup.
 logging.basicConfig(level=logging.INFO)
@@ -67,20 +68,115 @@ def main():
         tsv_in = csv.reader(tsv_in, delimiter='\t')
 
         ## Process data.
+        first_line_p = True
+        i = 0
         for line in tsv_in:
-            count = len(line)
-            if count != 10:
-                die_screaming('malformed line: ' + '\t'.join(line))
+            i = i + 1
+            if first_line_p:
+                first_line_p = False
+                continue
             else:
+                count = len(line)
+                if count != 10:
+                    die_screaming('malformed line: '+ str(i) +' '+ '\t'.join(line))
+                else:
 
-                ## TODO: Transform the comma/pipe-separated data
-                ## section into something usable.
+                    # print("-------")
+                    # print(type(line[3]))
+                    # print(len(line[3]))
+                    # print(line[3])
 
-                ## TODO: Now that we have that parsed, create a new version
-                ## of the previous column with mustache renderable
-                ## data hints.
+                    ## Base parsing everything into a common object.
+                    data_object = {}
+                    data_object["level"] = str(line[0]) # req
+                    data_object["chapter"] = str(line[1]) # req
+                    data_object["raw-japanese"] = str(line[2]) # req
+                    data_object["raw-ruby"] = line[3] if (type(line[3]) is str and len(line[3]) > 0) else None # opt
+                    data_object["reading"] = str(line[4]) # req
+                    data_object["meaning"] = line[5] # req
+                    data_object["section"] = line[6] # req
+                    data_object["extra"] = True if (type(line[7]) is str and line[7] == '*') else None # opt
+                    data_object["grammar-point"] = line[8] if (type(line[8]) is str and len(line[8]) > 0) else None # opt
+                    data_object["notes"] = line[9] if (type(line[9]) is str and len(line[9]) > 0) else None # opt
 
-                data_list.append(line)
+                    # print(data_object["raw-ruby"])
+
+                    ## Transform the comma/pipe-separated data raw "Ruby"
+                    ## object into something usable, if extant.
+                    ruby = []
+                    if data_object["raw-ruby"]:
+                        ruby_set_list_raw = data_object["raw-ruby"].split(",")
+                        for ruby_set_raw in ruby_set_list_raw:
+                            ruby_set_pre = ruby_set_raw.strip()
+                            # print(ruby_set_pre)
+                            ruby_set = ruby_set_pre.split("|")
+                            ruby_kanji = ruby_set[0].strip()
+                            ruby_reading = ruby_set[1].strip()
+                            ruby.append({"kanji": ruby_kanji,
+                                         "reading": ruby_reading})
+                    data_object["ruby"] = ruby
+
+                    ## Now that we have that parsed, create a new
+                    ## version of the "Japanese" ("raw-japanese")
+                    ## column with mustache renderable data hints.
+                    print('^^^')
+                    j = data_object["raw-japanese"]
+                    remaining_rubys = len(ruby)
+                    ruby_parse_data = []
+                    for r in ruby:
+                        ## Case when kanji not found in remaining
+                        ## japanese.
+                        print("japanese: " + j)
+                        print("kanji: " + r["kanji"])
+                        print("reading: " + r["reading"])
+                        if j.find(r["kanji"]) == -1:
+                            print('malformed line: '+ str(i) +' '+ '\t'.join(line))
+                            die_screaming('bad japanese/ruby at line '+ str(i))
+                        else:
+
+                            ## Some numbers we'll want on hand.
+                            jl = len(j) # the remaining length of the japanese
+                            rl = len(r["kanji"]) # the length of the ruby
+                            offset = j.find(r["kanji"]) # the offset of the kanji
+                            print(str(jl))
+                            print(str(rl))
+                            print(str(offset))
+
+                            ## Get the pre-ruby string added, if
+                            ## extant.
+                            if offset == 0:
+                                pass
+                            else:
+                                pre_string = j[0:(offset-1)]
+                                print('pre_string: ' + pre_string)
+                                ruby_parse_data.append({"string": pre_string})
+
+                            ## Add the ruby string section.
+                            ruby_string = j[offset:(offset+rl-1)]
+                            print('ruby_string: ' + ruby_string)
+                            ruby_parse_data.append({"string": ruby_string,
+                                                    "ruby":r["reading"]})
+
+                            ## If this is the last ruby we're dealing
+                            ## with, we're done and add the rest of
+                            ## the string. Otherwise, "soft loop" on
+                            ## the shorter string and next ruby.
+                            remaining_rubys = remaining_rubys - 1
+                            if remaining_rubys == 0:
+                                ## Last one, add any remaining string.
+                                if (offset+rl) < jl:
+                                    post_string = j[(offset+rl):jl]
+                                    print('post_string: ' + post_string)
+                                    ruby_parse_data.append({"string": post_string})
+                            else:
+                                j = j[(offset+rl):jl]
+
+                    data_object["rich-japanese"] = ruby_parse_data
+
+                    ## Onto the pile.
+                    data_list.append(data_object)
+
+    print(json.dumps(data_list, indent = 4))
 
     ## Write everything out in our given format.
     rendered = pystache.render(output_template, data_list)
