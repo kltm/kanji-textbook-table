c97b099649f3072aebb89933b9f6eda54c9b6b4e
Seth Carbon <github.com@genkisugi.net>
2019-08-17 14:27:34 -0700

    generalize the chapter and sections sorter into something more general

--- chapter-bin.py
+++ chapter-bin.py
@@ -6,7 +6,7 @@
 ####  python3 chapter-bin.py --help
 ####
 #### Get report of current problems and/or bin:
-####  python3 chapter-bin.py --input /tmp/input.json --output /tmp/output.json
+####  python3 chapter-bin.py --pattern vocab-list --input /tmp/input.json --output /tmp/output.json
 ####
 
 import sys
@@ -34,6 +34,8 @@ def main():
                         help='More verbose output')
     parser.add_argument('-i', '--input',
                         help='The file to use as input')
+    parser.add_argument('-p', '--pattern',
+                        help='The input-specific pattern that we need to use to bin the output')
     parser.add_argument('-o', '--output',
                         help='The file to output')
     args = parser.parse_args()
@@ -47,9 +49,26 @@ def main():
     if not args.input:
         die_screaming('need an input argument')
     LOGGER.info('Will input from: ' + args.input)
+    if not args.pattern:
+        die_screaming('need a pattern argument')
+    if args.pattern not in ["kanji-list", "vocab-list"]:
+        die_screaming('pattern argument unknown')
+    LOGGER.info('Will input from: ' + args.input)
     if not args.output:
         die_screaming('need an output argument')
     LOGGER.info('Will output to: ' + args.output)
+
+    ## Define the upper and lower sorting criteria.
+    if args.pattern == "vocab-list":
+        upper_set_field = "chapter"
+        section_field = "section"
+        section_field_order = [None, "読み物　一", "会話　一", "読み物　二", "会話　二", "読み物　三", "会話　三", "読み物　四", "会話　四"]
+    elif args.pattern == "kanji-list":
+        upper_set_field = "chapter"
+        section_field = "read-write"
+        section_field_order = ["W", "R"]
+    else:
+        die_screaming('unknown ordering pattern')
 
     ## Bring data in.
     data_list = []
@@ -57,50 +76,49 @@ def main():
         data_list = json.load(json_in_f)
 
     ## Sort the data into the different chapter sets.
-    chapter_sets = {}
+    upper_sets = {}
     for item in data_list:
-        chapter = str(item["chapter"])
+        chapter = str(item[upper_set_field])
         #print(chapter)
-        if not chapter in chapter_sets:
-            chapter_sets[chapter] = []
-        chapter_sets[chapter].append(item)
-    #print(", ".join(sorted(chapter_sets.keys(), key=int)))
+        if not chapter in upper_sets:
+            upper_sets[chapter] = []
+        upper_sets[chapter].append(item)
+    #print(", ".join(sorted(upper_sets.keys(), key=int)))
 
     ## Loop over the different chapters to create the output.
-    sectioned_chapter_sets = []
-    for chi in sorted(chapter_sets.keys(), key=int):
-        data_list = chapter_sets[chi]
+    sectioned_upper_sets = []
+    for chi in sorted(upper_sets.keys(), key=int):
+        data_list = upper_sets[chi]
 
-        ## Sort the chapter sets into sections sets.
+        ## Sort the upper/chapter sets into sections sets.
         sections = {}
         for item in data_list:
-            section = item["section"]
+            section = item[section_field]
             if not section in sections:
                 sections[section] = []
             sections[section].append(item)
 
         ## Manually add the sections in the order we want them to
         ## appear in the chapter.
-        section_order = [None, "読み物　一", "会話　一", "読み物　二", "会話　二", "読み物　三", "会話　三", "読み物　四", "会話　四"]
         ## Cross-check against what we have.
-        print(", ".join(sorted([str(x) for x in section_order])))
+        print(", ".join(sorted([str(x) for x in section_field_order])))
         print(", ".join(sorted([str(x) for x in sections.keys()])))
-        if not set(sections.keys()).issubset(set(section_order)):
+        if not set(sections.keys()).issubset(set(section_field_order)):
             die_screaming('unorderable section header')
 
         ## Prepare sections with(out) headers for final rendering as
         ## separate tables in the chapter docs.
         sectioned_data_list = []
-        for s in section_order:
+        for s in section_field_order:
             if s in sections:
-                sectioned_data_list.append({"header": s, "words": sections[s]})
+                sectioned_data_list.append({"header": s, "sections": sections[s]})
 
-        sectioned_chapter_sets.append({"chapter": str(chi), "data": sectioned_data_list})
+        sectioned_upper_sets.append({upper_set_field: str(chi), "data": sectioned_data_list})
 
         ## Write everything out.
         print(json.dumps(sectioned_data_list, indent = 4))
         with open(args.output, 'w') as output:
-            output.write(json.dumps(sectioned_chapter_sets, indent = 4))
+            output.write(json.dumps(sectioned_upper_sets, indent = 4))
 
 ## You saw it coming...
 if __name__ == '__main__':
