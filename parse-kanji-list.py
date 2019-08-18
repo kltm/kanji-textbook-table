9bbd0fa02322ef6311213d405cbce69c86d4a1de
Seth Carbon <github.com@genkisugi.net>
2019-08-17 18:12:58 -0700

    inject overlapping into atominzed fields

--- parse-kanji-list.py
+++ parse-kanji-list.py
@@ -9,7 +9,7 @@
 ####  python3 parse-kanji-list.py --tsv ~/Downloads/UCSC中上級教科書_漢字・単語リスト\ -\ 漢字リス ト.tsv --output /tmp/parsed-kanji-list.json
 ####
 #### As part of a pipeline:
-####  python3 parse-kanji-list.py --tsv ~/Downloads/UCSC中上級教科書_漢字リスト\ -\ 漢字リス ト\(4\).tsv --output /tmp/parsed-kanji-list.json && python3 chapter-bin.py -v --input /tmp/parsed-vocab-list.json --output /tmp/chapters.json && python3 apply-to-chapters.py --input /tmp/chapters.json --template ./word-html-frame.template.html --output /tmp/chapter
+####  python3 parse-kanji-list.py --tsv ~/Downloads/UCSC中上級教科書_漢字・単語リスト\ -\ 漢字リスト.tsv --output /tmp/parsed-kanji-list.json && python3 chapter-bin.py -v --pattern kanji-list --input /tmp/parsed-kanji-list.json --output /tmp/chapters-kl.json && python3 apply-to-chapters.py --input /tmp/chapters-kl.json
 ####
 
 import sys
@@ -18,6 +18,7 @@ import logging
 import csv
 import pystache
 import json
+import functools
 import os
 
 ## Logger basic setup.
@@ -60,7 +61,7 @@ def main():
 
     ## Setup some general metadata checking for the different formats.
     required_total_columns = 12
-    required_columns = ["level", "chapter", "read-write", "kanji", "hiragana", "meaning"]
+    required_columns = ["level", "chapter", "read-write", "kanji-raw", "hiragana-raw", "meaning"]
 
     ## Bring on all data in one sweep, formatting and adding
     ## appropriate parts to internal format so that we can simply
@@ -101,14 +102,14 @@ def main():
                     data_object["level"] = str(line[0]) # req
                     data_object["chapter"] = str(line[1]) # req
                     data_object["read-write"] = line[2] if (type(line[2]) is str and line[2] in ["W", "R"]) else None # req
-                    data_object["kanji"] = str(line[3]) # req
-                    data_object["hiragana"] = str(line[4]) # req
+                    data_object["kanji-raw"] = str(line[3]) # req
+                    data_object["hiragana-raw"] = str(line[4]) # req
                     data_object["introduced"] = str(line[5]) # opt
                     data_object["kanji-new"] = str(line[6]) # opt
                     data_object["reading-new"] = str(line[7]) # opt
                     data_object["meaning"] = line[8] # req
                     data_object["section"] = line[9] if (type(line[9]) is str and len(line[9]) > 0) else None # opt
-                    data_object["kanji-replacement"] = str(line[10]) # opt
+                    data_object["kanji-sightings"] = str(line[10]) # opt
                     data_object["notes"] = line[11] if (type(line[11]) is str and len(line[11]) > 0) else None # opt
 
                     ## Basic error checking.
@@ -129,6 +130,81 @@ def main():
                     data_object["read-write-header"] = "読めなければいけない漢字"
                     if data_object["read-write"] == "W":
                         data_object["read-write-header"] = "書けなければいけない漢字"
+
+                    ## Atomize the two strings that will need detailed
+                    ## highlighting.
+                    data_object["kanji-atomized"] = [{"chr": x} for x in list(data_object["kanji-raw"])]
+
+                    ## Underlining for hiragana. Start by atomizing it.
+                    hm = [{"chr": x} for x in list(data_object["hiragana-raw"])]
+                    data_object["hiragana-atomized"] = hm
+                    hr = data_object["hiragana-raw"]
+                    rn = data_object["reading-new"]
+                    if hr and rn:
+                        if not hr.find(rn) == -1:
+                                spoint = hr.find(rn)
+                                epoint = hr.find(rn) + len(rn)
+                                data_object["hiragana-atomized"].insert(epoint, {"token-underline-end-p": True})
+                                data_object["hiragana-atomized"].insert(spoint, {"token-underline-start-p": True})
+
+                    ## Underlining for kaniji; a little more
+                    ## complicated as we need to do two at the same
+                    ## time. Start by atomizing it.
+                    points_list = []
+                    data_object["kanji-atomized"] = [{"chr": x} for x in list(data_object["kanji-raw"])]
+                    kr = data_object["kanji-raw"]
+                    kn = data_object["kanji-new"]
+                    ki = data_object["introduced"] # kanji introduced
+                    if kr and kn:
+                        if not kr.find(kn) == -1:
+                                kn_start_point = kr.find(kn)
+                                kn_end_point = kr.find(kn) + len(kn)
+                                points_list.append({"point": kn_end_point,
+                                                    "type": "new",
+                                                    "pos": "end",
+                                                    "token": "token-bold-end-p"})
+                                points_list.append({"point": kn_start_point,
+                                                    "type": "new",
+                                                    "pos": "start",
+                                                    "token": "token-bold-start-p"})
+                    if kr and ki:
+                        if not kr.find(ki) == -1:
+                                ki_start_point = kr.find(ki)
+                                ki_end_point = kr.find(ki) + len(ki)
+                                points_list.append({"point": ki_end_point,
+                                                    "type": "intro",
+                                                    "pos": "end",
+                                                    "token": "token-dot-end-p"})
+                                points_list.append({"point": ki_start_point,
+                                                    "type": "intro",
+                                                    "pos": "start",
+                                                    "token": "token-dot-start-p"})
+                    ## Order the points list using a custom comparison
+                    ## to make sure that the overlaps are symmetric
+                    ## the way we want.
+                    def points_sort(b, a):
+                        if a["point"] > b["point"]:
+                            return 1
+                        elif a["point"] < b["point"]:
+                            return -1
+                        else:
+                            scale = {"new-start": 1,
+                                     "intro-start": 2,
+                                     "intro-end": 3,
+                                     "new-end": 4}
+                            bstr = b["type"] + "-" + b["pos"]
+                            astr = a["type"] + "-" + a["pos"]
+                            if scale[bstr] - scale[astr] > 0:
+                                return 1
+                            else:
+                                return -1
+                    sorted_points_list = sorted(points_list, key=functools.cmp_to_key(points_sort))
+                    #data_object["points-list"] = sorted_points_list
+
+                    ## Finally, inject the points into the atomized
+                    ## kanji.
+                    for point in sorted_points_list:
+                        data_object["kanji-atomized"].insert(point["point"], {point["token"]: True})
 
                     ## Onto the pile.
                     data_list.append(data_object)
